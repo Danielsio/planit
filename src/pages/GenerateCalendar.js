ce5160cd431f59b786ad6a5cc3674ee4349b2aad
kingd <kingddd301@gmail.com>
2023-03-03 22:35:29 +0200

    updated datepicker values to send long unix timestamp

--- src/pages/GenerateCalendar.js
+++ src/pages/GenerateCalendar.js
@@ -4,8 +4,6 @@ import "react-datepicker/dist/react-datepicker.css";
 import api from "../api/axiosBackendConfig";
 
 const GenerateCalendar = () => {
-  const options = { timeZone: "Asia/Jerusalem" };
-
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -16,34 +14,29 @@ const GenerateCalendar = () => {
   const handleGenerate = () => {
     validateDatesPicked();
 
-    const isoStartDate = new Date(
-      startDate.toLocaleString("en-US", options)
-    ).toISOString();
+    const start = startDate.getTime(); // convert to Unix timestamp
+    const end = endDate.getTime(); // convert to Unix timestamp
 
-    const isoEndDate = new Date(
-      endDate.toLocaleString("en-US", options)
-    ).toISOString();
+    console.log(start);
+    console.log(end);
 
-    console.log(isoStartDate);
-    console.log(isoEndDate);
-
-    // api
-    //   .post(
-    //     "/scan",
-    //     {},
-    //     {
-    //       params: {
-    //         start: startDate,
-    //         end: endDate,
-    //       },
-    //     }
-    //   )
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+    api
+      .post(
+        "/scan",
+        {},
+        {
+          params: {
+            start: start,
+            end: end,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -55,13 +48,8 @@ const GenerateCalendar = () => {
         <DatePicker
           selected={startDate}
           onChange={(date) => setStartDate(date)}
-          dateFormat="MM/dd/yyyy"
         />
-        <DatePicker
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-          dateFormat="MM/dd/yyyy"
-        />
+        <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
         <button onClick={handleGenerate}>Generate Calendar</button>
       </div>
     </>
