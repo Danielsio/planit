3008dbf9927e6e67ba6c06837d61d40a62051b58
kingd <kingddd301@gmail.com>
2023-03-06 23:12:48 +0200

    added some text to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,14 +2,35 @@ import React from "react";
 
 function About() {
   return (
-    <>
+    <div className="about-text">
       <h1>PlanIt</h1>
       <p>
-        A React app that servers as a Front-End to help students make the best
-        of their exams period.
+        A company that creates personalized curricula for students is one that
+        recognizes the unique learning needs and styles of each individual
+        student.
+      </p>
+      <p>
+        Rather than adhering to a one-size-fits-all approach, this company takes
+        into account a student's interests, strengths, and weaknesses to craft a
+        curriculum that maximizes their potential.
+      </p>
+      <p>
+        With personalized curricula, students can learn at their own pace, focus
+        on areas where they need more support, and explore topics that excite
+        them.
+      </p>
+      <p>
+        This approach can lead to improved academic performance, greater
+        engagement and motivation, and a more fulfilling learning experience
+        overall.{" "}
+      </p>
+      <p>
+        A company that specializes in personalized curricula demonstrates a
+        commitment to student success and can make a significant impact on the
+        lives of the students they serve.
       </p>
       <p>Version 1.0.0</p>
-    </>
+    </div>
   );
 }
 
